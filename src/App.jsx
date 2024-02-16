4d08a7ff133d1c5c3513efd725b16129805341fd
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2024-02-16 22:11:09 +0600

    user details update

--- src/App.jsx
+++ src/App.jsx
@@ -31,62 +31,55 @@ function App() {
     <>
       <div className="container">
         <div className="row">
-        <div className="col-md-6">
-          {loading ? (
-            <Spinner animation="border" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </Spinner>
-          ) : users.length > 0 ? (
-            <div className="list-group">
-              {users.map((user) => (
-                <div
-                  key={user.id}
-                  className="list-group-item "
-                  onClick={() => handleUser(user)}
-                >
-                      <div className="d-flex justify-content-center align-items-center">
-                        <img
-                          style={{
-                            width: "240px",
-                            height: "130px",
-                            borderRadius: "50%",
-                          }}
-                          src={user.avatar}
-                          alt="No data to show"
-                        />
-                      </div>
-                      <p>{user.profile.username}</p>
-                   
-                </div>
-              ))}
-            </div>
-          ) : null}
-        </div>
+          <div className="col-md-6">
+            {loading ? (
+              <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            ) : users.length > 0 ? (
+              <div className="list-group">
+                {users.map((user) => (
+                  <div
+                    key={user.id}
+                    className="list-group-item "
+                    onClick={() => handleUser(user)}
+                  >
+                    <div className="d-flex justify-content-center align-items-center">
+                      <img
+                        style={{
+                          width: "240px",
+                          height: "130px",
+                          borderRadius: "50%",
+                        }}
+                        src={user.avatar}
+                        alt="No data to show"
+                      />
+                    </div>
+                    <p>{user.profile.username}</p>
+                  </div>
+                ))}
+              </div>
+            ) : null}
+          </div>
 
-        <div className="col-md-6">
-          {selectedUsers && (
-            <Card>
-              <ListGroup variant="flush">
-                <ListGroup.Item>
-                  {selectedUsers.profile.firstName}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  {selectedUsers.profile.lastName}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  {selectedUsers.profile.email}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  {selectedUsers.Bio}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  {selectedUsers.jobTitle}
-                </ListGroup.Item>
-              </ListGroup>
-            </Card>
-          )}
+          <div className="col-md-6">
+            {selectedUsers && (
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    {selectedUsers.profile.firstName}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    {selectedUsers.profile.lastName}
+                  </ListGroup.Item>
+                  <ListGroup.Item>{selectedUsers.profile.email}</ListGroup.Item>
+                  {/* <ListGroup.Item>{selectedUsers.Bio}</ListGroup.Item> */}
+                  <ListGroup.Item>{selectedUsers.jobTitle}</ListGroup.Item>
+                </ListGroup>
+              </Card>
+            )}
+          </div>
         </div>
-      </div>
       </div>
     </>
   );
